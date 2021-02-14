5d864e483f8e9f6a01e5fc8f78ff24e08c57cf68
Juan Rebella <juan.rebella@gmail.com>
2021-02-14 18:43:54 -0300

    Comenzando con JS y DOM

---
+++ main.js
@@ -0,0 +1,34 @@
+ 
+numbers
+Strings
+Booleans : true or false
+Typeof
+
+
+alert("You have written: " + chars + ", you have: "+ chars + " left.");
+
+
+Tweet count example:
+
+var tweet = prompt("Create your tweet: ");
+var tweetCount = tweet.length;
+alert("You have written: "+ tweetCount +" characters. You have: "+ (140 - tweetCount) +" left to use.");
+
+Slice()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

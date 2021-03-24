05d0b60dbe1b61f3bee4574a119401c7a0d4aa56
Juan Rebella <juan.rebella@gmail.com>
2021-03-23 21:42:11 -0300

    more Express - Calculator examples

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,8 +1,22 @@
 //jshint esversion:6
 
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
-app.listen(5502, function () {
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/calculator.html");
+});
+
+
+app.post("/", function(req, res) {
+   
+    console.log(req.body);
+    res.send("Thanks for posting that!");  
+});
+
+app.listen(5502, function() {
     console.log("Server started on port 5502");
 });
